aeda1263ed0c32315ac1e56c3cabbb04a6368c3e
Omobayode6 <omobayode93@gmail.com>
2023-03-13 23:30:39 +0100

    develope faq page

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('links')[0]
 const contact = document.getElementsByClassName('btn')[0]
+const faqToggleButtons = document.querySelectorAll('.faq-toggle');
 
 console.log(navbarLinks)
 toggleButton.addEventListener('click', () =>{
   navbarLinks.classList.toggle('active');
   contact.classList.toggle('active');
 })
+
+
+faqToggleButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const answer = button.parentElement.nextElementSibling;
+    button.classList.toggle('active');
+    answer.classList.toggle('active');
+  });
+});

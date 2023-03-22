1316f2ba00b1259810e0ec7524b0125404dc7360
Omobayode6 <omobayode93@gmail.com>
2023-03-22 10:52:11 +0100

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('links')[0]
-const contact = document.getElementsByClassName('hearder-btn')[0]
+const navbarLinks = document.getElementsByClassName('links')[0];
+const contact = document.getElementsByClassName('hearder-btn')[0];
 const faqToggleButtons = document.querySelectorAll('.faq-toggle');
 
 console.log(navbarLinks)
@@ -9,11 +9,15 @@ toggleButton.addEventListener('click', () =>{
   contact.classList.toggle('active');
 })
 
-
 faqToggleButtons.forEach(button => {
   button.addEventListener('click', () => {
     const answer = button.parentElement.nextElementSibling;
     button.classList.toggle('active');
     answer.classList.toggle('active');
   });
+});
+
+
+$.getJSON("https://api.countapi.xyz/hit/omobayode6.github.io/kura//visits", function(response) {
+    $("#visits").text(response.value);
 });
